3ed39f6a9606c45794c452f4167c0e800d32c880
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2023-10-27 11:15:49 +0530

    remove config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-export default{
-    JWT_SECRET:"FL7BdZDNRBrcV3j8ECoq77kGHka7797Dnqn84AdktXQ=",
-    EMAIL:"	rosalind.jacobi@ethereal.email",
-    PASSWORD:"rwbcbdaZSQx6xW9GWK",
-    ATLAS_URL: "mongodb+srv://rayanthilakshana2000:admin123@mern-app.oswd6mj.mongodb.net/?retryWrites=true&w=majority"
-}
